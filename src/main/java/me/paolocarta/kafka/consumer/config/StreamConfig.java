5fb5800475ef70d5f1c750decca70aba7afc28e8
Paolo Carta <ing.pcarta@gmail.com>
2020-05-19 19:48:10 +0200

    fixes

--- src/main/java/me/paolocarta/kafka/consumer/config/StreamConfig.java
+++ src/main/java/me/paolocarta/kafka/consumer/config/StreamConfig.java
@@ -1,8 +1,7 @@
-package me.paolocarta.kafka.producer.config;
+package me.paolocarta.kafka.consumer.config;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
